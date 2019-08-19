6d1f5ca8cdcae1ee83ea3de2e988be68a10facd0
Pramesh <pramesh@techsevin.com>
2019-08-19 15:00:05 +0530

    First Commit

---
+++ app/src/main/java/com/example/service_manager/MyService.java
@@ -0,0 +1,46 @@
+package com.example.service_manager;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    MediaPlayer myPlayer;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public void onCreate() {
+
+        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
+        myPlayer = MediaPlayer.create(this, R.raw.song);
+        myPlayer.setLooping(false); // Set looping
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Services Started ", Toast.LENGTH_SHORT).show();
+        myPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+    /*
+    --------------------This Below Method is Depricated -----------------------
+
+    @Override
+    public void onStart(Intent intent, int startid) {
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        myPlayer.start();
+    }*/
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
+        myPlayer.stop();
+    }
+}
